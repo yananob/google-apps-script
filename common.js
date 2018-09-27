8aa9a4fde390ea30166babf27b66d2e8477f2620
yananob <yananob@gmail.com>
2018-09-27 23:14:58 +0900

    Revert "Initial commit"
    This reverts commit ec590165f4ac7dec5c4ff192db046ff144a65717.

--- common.js
+++
@@ -1,7 +0,0 @@
-
-function sendLog_(app_name) {
-  var recipient = Session.getActiveUser().getEmail();
-  var subject = '[GASlog] ' + app_name;
-  var body = Logger.getLog();
-  MailApp.sendEmail(recipient, subject, body);
-}
